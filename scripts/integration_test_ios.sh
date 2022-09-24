875c69cbec124aadec92a5a9b03bb0a2f31f618c
ldakhoa <ldakhoa1308@gmail.com>
2022-09-25 01:47:19 +0700

    Change test scripts

--- scripts/integration_test_ios.sh
+++ scripts/integration_test_ios.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
+set -e
 
 cd examples/ios
+mkdir -p tmp && printf 0 > tmp/test_retries_trace # To simulate test retries
+
+if [[ ! -d SeedeeExample.xcworkspace || ! -d Pods ]]; then
+	bundle install
+	bundle exec pod install
+fi
+
+common_args=(
+    --derived-data-path DerivedData
+)
 
 function _exec_build() {
-
+	swift run seedee build \
+		${common_args[@]} \
+		--build-for-testing
 }
 
-function _exec_test() {
+# function _exec_test() {}
 
-}
+if [[ -z ${ACTION} ]]; then
+	actions=(${ACTION})
+	echo "test action"
+else
+	actions=(${ACTION})
+	echo "test action 2"
+fi
+
+for action in ${action[@]}; do
+	eval "_exec_${action}"
+done
