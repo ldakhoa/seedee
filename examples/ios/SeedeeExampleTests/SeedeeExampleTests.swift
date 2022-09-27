078ded28e75a3028c277c7dcfafd0b5a84342476
ldakhoa <ldakhoa1308@gmail.com>
2022-09-28 02:10:14 +0700

    Using Class for action & Add example Test schemes

---
+++ examples/ios/SeedeeExampleTests/SeedeeExampleTests.swift
@@ -0,0 +1,14 @@
+//
+//  SeedeeExampleTests.swift
+//  SeedeeExampleTests
+//
+//  Created by Khoa Le on 28/09/2022.
+//
+
+import XCTest
+@testable import SeedeeExample
+
+final class SeedeeExampleTests: XCTestCase {
+    func test_example1() throws {}
+    func test_example2() throws {}
+}
