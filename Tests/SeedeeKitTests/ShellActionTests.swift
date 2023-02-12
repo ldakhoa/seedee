cc8c71e0667a8356340deb37359b3d542277deb7
Khoa Le <ldakhoa1308@gmail.com>
2023-02-12 17:14:18 +0700

    Trimming white space to fix test

--- Tests/SeedeeKitTests/ShellActionTests.swift
+++ Tests/SeedeeKitTests/ShellActionTests.swift
@@ -4,17 +4,17 @@ import XCTest
 final class ShellActionTests: XCTestCase {
     func test_runShell() async throws {
         let commandBuilder = CommandBuilder("ls")
-        
+
         let action = ShellAction(commandBuilder: commandBuilder, workingDirectory: integrationAppPath)
         let result = try await action.run()
         XCTAssertTrue(result.output.contains("IntegrationApp"))
         XCTAssertTrue(result.output.contains("IntegrationApp.xcodeproj"))
         XCTAssertEqual(result.terminationStatus, 0)
     }
-    
+
     func test_runShell_fail() async throws {
         let commandBuilder = CommandBuilder("ll")
-        
+
         let action = ShellAction(commandBuilder: commandBuilder, workingDirectory: integrationAppPath)
         do {
             try await action.run()
