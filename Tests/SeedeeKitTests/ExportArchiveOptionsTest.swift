43175d916fadabdc95f484b0735b5e15dd02fb94
Khoa Le <ldakhoa1308@gmail.com>
2023-02-19 03:39:40 +0700

    Add unit test for `ExportArchiveOptions`

--- Tests/SeedeeKitTests/ExportArchiveOptionsTest.swift
+++ Tests/SeedeeKitTests/ExportArchiveOptionsTest.swift
@@ -3,25 +3,56 @@ import XCTest
 @testable import SeedeeKit
 
 final class ExportArchiveOptionsTests: XCTestCase {
+    var manifest: ExportArchiveOptions.Manifest!
 
-    func test_buildPropertyList() throws {
-        // given
+    override func setUp() {
+        super.setUp()
+
+        manifest = ExportArchiveOptions.Manifest(
+            appURL: "https://seedee.com/appurl",
+            displayImageURL: "https://seedee.com/displayimageurl",
+            fullSizeImageURL: "https://seedee.com/fullsizeimageurl")
+    }
+
+    func test_buildPropertyList_whenDistributionIsNonAppStore_andSiginingAutomatic() async throws {
         let options = ExportArchiveOptions(
-            compileBitcode: true,
-            destination: .upload,
-            method: .development,
-            signingStyle: .manual,
-            stripSwiftSymbols: true,
-            teamID: "12345678",
-            uploadSymbols: true,
-            manageAppVersionAndBuildNumber: true,
-            distributionBundleIdentifier: "com.example.myapp",
-            provisinioningProfile: ["UUID": "MyProvisioningProfile"])
+            method: .adHoc,
+            distribution: .nonAppStore(
+                manifest: manifest,
+                compileBitcode: false,
+                onDemandResources: nil,
+                thinning: .none
+            ),
+            signing: .automatic,
+            distributionBundleIdentifier: "com.example.app",
+            iCloudContainerEnvironment: "Production",
+            stripSwiftSymbols: false,
+            teamID: "ABCDE12345"
+        )
 
-        // when
+        let expected: [String: Any] = [
+            "signingStyle": "automatic",
+            "embedOnDemandResourcesAssetPacksInBundle": true,
+            "stripSwiftSymbols": false,
+            "manageAppVersionAndBuildNumber": false,
+            "manifest": [
+                "appURL": "https://seedee.com/appurl",
+                "displayImageURL": "https://seedee.com/displayimageurl",
+                "fullSizeImageURL": "https://seedee.com/fullsizeimageurl"
+            ],
+            "uploadSymbols": false,
+            "method": "ah-hoc",
+            "teamID": "ABCDE12345",
+            "thinning": "<none>",
+            "destination": "export",
+            "compileBitcode": false,
+            "distributionBundleIdentifier": "com.example.app",
+            "generateAppStoreInformation": false,
+            "iCloudContainerEnvironment": "Production"
+        ]
+
         let plistData = try options.buildPropertyList()
 
-        // then
         let plist = try PropertyListSerialization.propertyList(
             from: plistData,
             options: [],
@@ -29,15 +60,143 @@ final class ExportArchiveOptionsTests: XCTestCase {
         ) as? [String: Any]
 
         XCTAssertNotNil(plist)
-        XCTAssertEqual(plist?["compileBitcode"] as? Bool, true)
-        XCTAssertEqual(plist?["destination"] as? String, "upload")
-        XCTAssertEqual(plist?["method"] as? String, "development")
-        XCTAssertEqual(plist?["signingStyle"] as? String, "manual")
-        XCTAssertEqual(plist?["stripSwiftSymbols"] as? Bool, true)
-        XCTAssertEqual(plist?["teamID"] as? String, "12345678")
-        XCTAssertEqual(plist?["uploadSymbols"] as? Bool, true)
-        XCTAssertEqual(plist?["manageAppVersionAndBuildNumber"] as? Bool, true)
-        XCTAssertEqual(plist?["distributionBundleIdentifier"] as? String, "com.example.myapp")
-        XCTAssertEqual(plist?["provisinioningProfile"] as? [String: String], ["UUID": "MyProvisioningProfile"])
+        XCTAssertEqual(plist! as NSDictionary, expected as NSDictionary)
+    }
+
+    func test_buildPropertyList_whenDistributionIsNonAppStore_andSiginingManual() async throws {
+        let additionalOptions = ExportArchiveOptions.BuiltInSigning.AdditionalSigningOptions(signingCertificate: .sha1(hash: "mock.hash.ExportArchiveOptions.BuiltInSigning.AdditionalSigningOptions"))
+        let options = ExportArchiveOptions(
+            method: .adHoc,
+            distribution: .nonAppStore(
+                manifest: manifest,
+                compileBitcode: false,
+                onDemandResources: nil,
+                thinning: .none
+            ),
+            signing: .manual(
+                provisioningProfile: ["provisioningProfileKey": "provisioningProfileValue"],
+                additionalSigningOptions: additionalOptions),
+            distributionBundleIdentifier: "com.example.app",
+            iCloudContainerEnvironment: "Production",
+            stripSwiftSymbols: false,
+            teamID: "ABCDE12345"
+        )
+
+        let expected: [String: Any] = [
+            "signingStyle": "manual",
+            "embedOnDemandResourcesAssetPacksInBundle": true,
+            "stripSwiftSymbols": false,
+            "manageAppVersionAndBuildNumber": false,
+            "manifest": [
+                "appURL": "https://seedee.com/appurl",
+                "displayImageURL": "https://seedee.com/displayimageurl",
+                "fullSizeImageURL": "https://seedee.com/fullsizeimageurl"
+            ],
+            "provisioningProfiles": ["provisioningProfileKey": "provisioningProfileValue"],
+            "signingCertificate": "mock.hash.ExportArchiveOptions.BuiltInSigning.AdditionalSigningOptions",
+            "uploadSymbols": false,
+            "method": "ah-hoc",
+            "teamID": "ABCDE12345",
+            "thinning": "<none>",
+            "destination": "export",
+            "compileBitcode": false,
+            "distributionBundleIdentifier": "com.example.app",
+            "generateAppStoreInformation": false,
+            "iCloudContainerEnvironment": "Production"
+        ]
+
+        let plistData = try options.buildPropertyList()
+
+        let plist = try PropertyListSerialization.propertyList(
+            from: plistData,
+            options: [],
+            format: nil
+        ) as? [String: Any]
+
+        XCTAssertNotNil(plist)
+        XCTAssertEqual(plist! as NSDictionary, expected as NSDictionary)
+    }
+
+    func test_buildPropertyList_whenDistributionIsAppStore_andSiginingAutomatic() async throws {
+        let options = ExportArchiveOptions(
+            method: .development,
+            distribution: .appStore(uploadToAppStore: true),
+            signing: .automatic,
+            distributionBundleIdentifier: "com.example.app",
+            iCloudContainerEnvironment: "Production",
+            stripSwiftSymbols: false,
+            teamID: "ABCDE12345"
+        )
+
+        let plistData = try options.buildPropertyList()
+
+        let plist = try PropertyListSerialization.propertyList(
+            from: plistData,
+            options: [],
+            format: nil
+        ) as? [String: Any]
+
+        let expected: [String: Any] = [
+            "signingStyle": "automatic",
+            "teamID": "ABCDE12345",
+            "thinning": "<none>",
+            "embedOnDemandResourcesAssetPacksInBundle": true,
+            "iCloudContainerEnvironment": "Production",
+            "generateAppStoreInformation": false,
+            "manageAppVersionAndBuildNumber": true,
+            "uploadSymbols": true,
+            "method": "development",
+            "destination": "upload",
+            "compileBitcode": true,
+            "distributionBundleIdentifier": "com.example.app",
+            "stripSwiftSymbols": false
+        ]
+
+        XCTAssertNotNil(plist)
+        XCTAssertEqual(plist! as NSDictionary, expected as NSDictionary)
+    }
+
+    func test_buildPropertyList_whenDistributionIsAppStore_andSiginingManual() async throws {
+        let additionalOptions = ExportArchiveOptions.BuiltInSigning.AdditionalSigningOptions(signingCertificate: .sha1(hash: "mock.hash.ExportArchiveOptions.BuiltInSigning.AdditionalSigningOptions"))
+        let options = ExportArchiveOptions(
+            method: .adHoc,
+            distribution: .appStore(uploadToAppStore: true),
+            signing: .manual(
+                provisioningProfile: ["provisioningProfileKey": "provisioningProfileValue"],
+                additionalSigningOptions: additionalOptions),
+            distributionBundleIdentifier: "com.example.app",
+            iCloudContainerEnvironment: "Production",
+            stripSwiftSymbols: false,
+            teamID: "ABCDE12345"
+        )
+
+        let expected: [String: Any] = [
+            "signingStyle": "manual",
+            "compileBitcode": true,
+            "signingCertificate": "mock.hash.ExportArchiveOptions.BuiltInSigning.AdditionalSigningOptions",
+            "generateAppStoreInformation": false,
+            "provisioningProfiles": ["provisioningProfileKey": "provisioningProfileValue"],
+            "teamID": "ABCDE12345",
+            "iCloudContainerEnvironment": "Production",
+            "method": "ah-hoc",
+            "manageAppVersionAndBuildNumber": true,
+            "uploadSymbols": true,
+            "distributionBundleIdentifier": "com.example.app",
+            "thinning": "<none>",
+            "embedOnDemandResourcesAssetPacksInBundle": true,
+            "destination": "upload",
+            "stripSwiftSymbols": false
+        ]
+
+        let plistData = try options.buildPropertyList()
+
+        let plist = try PropertyListSerialization.propertyList(
+            from: plistData,
+            options: [],
+            format: nil
+        ) as? [String: Any]
+
+        XCTAssertNotNil(plist)
+        XCTAssertEqual(plist! as NSDictionary, expected as NSDictionary)
     }
 }
