1533f01ab5e1d8bb134b5115f6dfb3f374f30e95
Khoa Le <ldakhoa1308@gmail.com>
2023-03-07 14:37:36 +0700

    Fix swiftlint

--- Tests/SeedeeKitTests/TestXcodeProjectActionTests.swift
+++ Tests/SeedeeKitTests/TestXcodeProjectActionTests.swift
@@ -10,31 +10,31 @@ final class TestXcodeProjectActionTests: XCTestCase {
             commandBuilder: CommandBuilder("bundle install && bundle exec pod install"),
             workingDirectory: fixturePath(for: "IntegrationPodApp")
         )
-        
+
         try await action.run()
     }
-    
+
     func test_testXcodeProject_buildShouldSuccess() async throws {
         let project = Project(
             workingDirectory: integrationAppPath,
             projectPath: "IntegrationApp.xcodeproj",
             scheme: "IntegrationApp")
-        
+
         let action = TestXcodeProjectAction(project: project)
-        
+
         let result = try await action.run()
         XCTAssertEqual(result.terminationStatus, 0)
         XCTAssertTrue(result.output.lowercased().contains("** test succeeded **"))
     }
-    
+
     func test_testXcodeProject_buildShouldFail() async throws {
         let project = Project(
             workingDirectory: integrationAppPath,
             projectPath: "IntegrationAppWrongName.xcodeproj",
             scheme: "IntegrationApp")
-        
+
         let action = TestXcodeProjectAction(project: project)
-        
+
         do {
             try await action.run()
             XCTFail("It should be fail")
